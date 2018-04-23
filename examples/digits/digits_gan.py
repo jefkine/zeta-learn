6e80f5b094674cb7f127069746cae8da9367d1d6
Jefkine <jefkine@gmail.com>
2018-04-23 07:37:34 +0300

    updates to gan model

--- examples/digits/digits_gan.py
+++ examples/digits/digits_gan.py
@@ -23,7 +23,7 @@ half_batch = int(batch_size * 0.5)
 verbose = True
 init_type = 'he_uniform'
 
-model_epochs = 5000
+model_epochs = 8000
 model_stats = {"d_train_loss": [], "d_train_acc": [], "g_train_loss": [], "g_train_acc": []}
 
 d_opt = register_opt(optimizer_name = 'adam', beta1 = 0.5, learning_rate = 0.00001)
@@ -32,13 +32,13 @@ g_opt = register_opt(optimizer_name = 'adam', beta1 = 0.5, learning_rate = 0.000
 def stack_generator_layers(init):
     model = Sequential(init_method = init)
     model.add(Dense(128, input_shape = (latent_dim,)))
-    model.add(Activation('leaky_relu', alpha = 0.2))
+    model.add(Activation('relu'))
     model.add(BatchNormalization(momentum = 0.8))
     model.add(Dense(256))
-    model.add(Activation('leaky_relu', alpha = 0.2))
+    model.add(Activation('relu'))
     model.add(BatchNormalization(momentum = 0.8))
     model.add(Dense(512))
-    model.add(Activation('leaky_relu', alpha = 0.2))
+    model.add(Activation('relu'))
     model.add(BatchNormalization(momentum = 0.8))
     model.add(Dense(img_dim, activation = 'tanh'))
 
@@ -58,11 +58,11 @@ def stack_discriminator_layers(init):
 
 # stack and compile the generator
 generator = stack_generator_layers(init = init_type)
-generator.compile(loss = 'bce', optimizer = g_opt)
+generator.compile(loss = 'cce', optimizer = g_opt)
 
 # stack and compile the discriminator
 discriminator = stack_discriminator_layers(init = init_type)
-discriminator.compile(loss = 'bce', optimizer = d_opt)
+discriminator.compile(loss = 'cce', optimizer = d_opt)
 
 # stack and compile the generator_discriminator
 generator_discriminator = Sequential(init_method = init_type)
@@ -78,7 +78,7 @@ for epoch_idx in range(model_epochs):
     # set the discriminator to trainable
     discriminator.trainable = True
 
-    for epoch_k in range(5):
+    for epoch_k in range(10):
 
         # draw random samples from real images
         index = np.random.choice(images.shape[0], half_batch, replace = False)
@@ -97,11 +97,14 @@ for epoch_idx in range(model_epochs):
         # discriminator training
         d_loss_real, d_acc_real = discriminator.train_on_batch(imgs, d_valid)
         d_loss_fake, d_acc_fake = discriminator.train_on_batch(gen_imgs, d_fake)
+        
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        d_acc = 0.5 * np.add(d_acc_real, d_acc_fake)
+        
+        if verbose:
+            print('Epoch {} K:{} Discriminator Loss: {:2.4f}, Acc: {:2.4f}.'.format(epoch_idx+1, epoch_k+1, d_loss, d_acc))
 
     # end of for epoch_k in range(1):
-
-    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-    d_acc = 0.5 * np.add(d_acc_real, d_acc_fake)
 
     model_stats["d_train_loss"].append(d_loss)
     model_stats["d_train_acc"].append(d_acc)
@@ -125,8 +128,9 @@ for epoch_idx in range(model_epochs):
         computebar(model_epochs, epoch_idx)
     else:
         # print the progress
-        print('Epoch {} Discriminator Loss: {:2.4f}, Acc: {:2.4f}.'.format(epoch_idx, d_loss, d_acc))
-        print('Epoch {} Generator Loss: {:2.4f}, Acc: {:2.4f}.\n'.format(epoch_idx, g_loss, g_acc))
+        print_epoch = epoch_idx + 1
+        print('\nEpoch {} Discriminator Loss: {:2.4f}, Acc: {:2.4f}.'.format(print_epoch, d_loss, d_acc))
+        print('Epoch {} Generator Loss: {:2.4f}, Acc: {:2.4f}.\n'.format(print_epoch, g_loss, g_acc))
 
 plot_metric('Loss', model_epochs, model_stats['d_train_loss'], model_stats['g_train_loss'], legend = ['D', 'G'])
 plot_metric('Accuracy', model_epochs, model_stats['d_train_acc'], model_stats['g_train_acc'], legend = ['D', 'G'])
