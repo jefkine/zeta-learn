1ab8fac94f3db4d4f0852f0104185e7f3e2813c5
Jefkine <jefkine@gmail.com>
2018-04-26 06:42:08 +0300

    tab edits

--- ztlearn/utils/charts.py
+++ ztlearn/utils/charts.py
@@ -6,19 +6,19 @@ FONT_SIZE = 10
 FIG_SIZE = (7, 5)
 
 def plotter(x,
-            y = [],
-            plot_dict = {},
-            fig_dims = (7, 5),
-            xticks_dict = {},
-            yticks_dict = {},
-            title = 'Model',
-            title_dict = {},
-            ylabel = 'y',
-            ylabel_dict = {},
-            xlabel = 'x',
-            xlabel_dict = {},
-            legend = ['train', 'valid'],
-            legend_dict = {}):
+               y = [],
+               plot_dict = {},
+               fig_dims = (7, 5),
+               xticks_dict = {},
+               yticks_dict = {},
+               title = 'Model',
+               title_dict = {},
+               ylabel = 'y',
+               ylabel_dict = {},
+               xlabel = 'x',
+               xlabel_dict = {},
+               legend = ['train', 'valid'],
+               legend_dict = {}):
 
     plt.figure(figsize = fig_dims)
     for i in range(len(y)):
@@ -34,20 +34,21 @@ def plotter(x,
 
 
 def plot_metric(metric,
-                epoch,
-                train,
-                valid,
-                plot_dict = {'linewidth' : 0.8},
-                fig_dims = FIG_SIZE,
-                xticks_dict = {'size' : FONT_SIZE},
-                yticks_dict = {'size' : FONT_SIZE},
-                title_dict = {'size' : FONT_SIZE},
-                ylabel_dict = {'size' : FONT_SIZE},
-                xlabel_dict = {'size' : FONT_SIZE},
-                legend = ['train', 'valid'],
-                legend_dict = {'loc' : 'upper right'}):
-    
-    plt = plotter(range(epoch), [train, valid],
+                        epoch,
+                        train,
+                        valid,
+                        plot_dict = {'linewidth' : 0.8},
+                        fig_dims = FIG_SIZE,
+                        xticks_dict = {'size' : FONT_SIZE},
+                        yticks_dict = {'size' : FONT_SIZE},
+                        title_dict = {'size' : FONT_SIZE},
+                        ylabel_dict = {'size' : FONT_SIZE},
+                        xlabel_dict = {'size' : FONT_SIZE},
+                        legend = ['train', 'valid'],
+                        legend_dict = {'loc' : 'upper right'}):
+
+    plt = plotter(range(epoch),
+                                [train, valid],
                                 plot_dict = plot_dict,
                                 fig_dims = fig_dims,
                                 xticks_dict = xticks_dict,
@@ -71,7 +72,7 @@ def plot_digits_img_results(test_data, test_label, predictions, fig_dims = (6, 6
     for i in range(36):
         digit = fig.add_subplot(6, 6, i + 1, xticks = [], yticks = [])
         digit.imshow(test_data.reshape(-1, 8, 8)[i], cmap = plt.cm.binary, interpolation = 'nearest')
-        
+
         if predictions[i] == test_label[i]:
             digit.text(0, 7, str(predictions[i]), color = 'green')
         else:
@@ -106,17 +107,17 @@ def plot_digits_img_samples(data, fig_dims = (6, 6)):
 
 
 def plot_regression_results(train_data,
-                            train_label,
-                            test_data,
-                            test_label,
-                            input_data, 
-                            pred_line,
-                            mse, super_title,
-                            y_label,
-                            x_label,
-                            fig_dims = FIG_SIZE,
-                            font_size = 10):
-    
+                                        train_label,
+                                        test_data,
+                                        test_label,
+                                        input_data,
+                                        pred_line,
+                                        mse, super_title,
+                                        y_label,
+                                        x_label,
+                                        fig_dims = FIG_SIZE,
+                                        font_size = 10):
+
     plt.figure(figsize = fig_dims)
     cmap = plt.get_cmap('summer')
     train = plt.scatter(train_data, train_label, color = cmap(0.8), s = 12)
@@ -130,5 +131,5 @@ def plot_regression_results(train_data,
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.legend((train, test), ("Train", "Test"), loc='upper left')
-    
+
     plt.show()
