249a968ab2bdacfc59644409110eedb294fc5fd9
Jefkine <jefkine@gmail.com>
2018-06-16 01:00:38 +0300

    tab edits

--- ztlearn/activations.py
+++ ztlearn/activations.py
@@ -100,7 +100,8 @@ class SELU:
             numpy.array: the output of the SELU function applied to the input
         """
 
-        return SELU._LAMBDA * np.where(input_signal >= 0.0, input_signal,
+        return SELU._LAMBDA * np.where(input_signal >= 0.0,
+                                                            input_signal,
                                                             np.multiply(SELU.ALPHA,
                                                             np.exp(input_signal)) - SELU.ALPHA)
 
