6b68fa680d2a0410a6aed5146ca3d565e121014f
Jefkine <jefkine@gmail.com>
2018-06-16 15:39:45 +0300

    edit comment

--- ztlearn/utils/numba_utils.py
+++ ztlearn/utils/numba_utils.py
@@ -8,4 +8,4 @@ DISABLE_JIT_FLAG = 0
 # jit options
 
 NOGIL_FLAG = True # enable or disable gil lock
-CACHE_FLAG = True # enable or disable cache of compiled lock
+CACHE_FLAG = True # enable or disable cache of compiled code
