4797f37857d81501acac7adce0621062fb678a08
Jefkine <jefkine@gmail.com>
2018-08-08 22:24:49 +0300

    tab edits

--- ztlearn/dl/layers/convolutional.py
+++ ztlearn/dl/layers/convolutional.py
@@ -76,7 +76,10 @@ class Conv(Layer):
                                                       self.kernel_size[0],
                                                       self.kernel_size[1])
 
-        output_height, output_width = get_output_dims(self.input_shape[1], self.input_shape[2], self.kernel_size, self.strides, self.padding)
+        output_height, output_width = get_output_dims(self.input_shape[1], self.input_shape[2],
+                                                                           self.kernel_size,
+                                                                           self.strides, 
+                                                                           self.padding)
 
         return self.filters, int(output_height), int(output_width)
 
