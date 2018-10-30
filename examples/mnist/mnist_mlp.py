bba15dfcddf301f4ef20f9d8ef1795faec7386f5
Jefkine <jefkine@gmail.com>
2018-10-31 02:54:36 +0300

    improved params tuning

--- examples/mnist/mnist_mlp.py
+++ examples/mnist/mnist_mlp.py
@@ -12,13 +12,13 @@ train_data, test_data, train_label, test_label = train_test_split(mnist.data,
                                                                   mnist.target.astype('int'),
                                                                   test_size   = 0.3,
                                                                   random_seed = 15,
-                                                                  cut_off     = 5000)
+                                                                  cut_off     = None)
 
 # plot samples of training data
 plot_tiled_img_samples(train_data[:40], train_label[:40], dataset = 'mnist')
 
 # optimizer definition
-opt = register_opt(optimizer_name = 'adam', momentum = 0.01, learning_rate = 0.001)
+opt = register_opt(optimizer_name = 'nestrov', momentum = 0.01, learning_rate = 0.0001)
 
 # model definition
 model = Sequential()
