66a62b70074c8e8df443f1f8dccdc6f6251d3ead
Jefkine <jefkine@gmail.com>
2018-12-09 17:28:54 +0300

    updated datasets module

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+# import file(s)
 from . import iris
 from . import mnist
 from . import digits
 from . import fashion
+from . import data_set
+
+# import from data_utils.py
+from .data_set import DataSet
