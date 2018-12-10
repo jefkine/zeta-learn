26cd190bfdd353f5bd062615cc61e0d09c3ad86f
Jefkine <jefkine@gmail.com>
2018-12-10 16:34:25 +0300

    tab edits

--- examples/iris/iris_mlp.py
+++ examples/iris/iris_mlp.py
@@ -7,11 +7,9 @@ from ztlearn.optimizers import register_opt
 from ztlearn.datasets.iris import fetch_iris
 
 data = fetch_iris()
-# print(data.describe)
-
 train_data, test_data, train_label, test_label = train_test_split(data.data,
                                                                   data.target,
-                                                                  test_size   = 0.3, 
+                                                                  test_size   = 0.3,
                                                                   random_seed = 5)
 
 # optimizer definition
