939c2663bae1dedab92db585fa1bd39e65f0e075
Jefkine <jefkine@gmail.com>
2018-12-16 13:49:03 +0300

    tab edits

--- examples/sequences/seq_rnn.py
+++ examples/sequences/seq_rnn.py
@@ -34,5 +34,5 @@ fit_stats = model.fit(train_data,
 print_seq_results(model.predict(test_data), test_label, test_data)
 
 model_name = model.model_name
-plot_metric('loss',     model_epochs, fit_stats['train_loss'], fit_stats['valid_loss'], model_name = model_name)
-plot_metric('accuracy', model_epochs, fit_stats['train_acc'],  fit_stats['valid_acc'],  model_name = model_name)
+plot_metric('loss', model_epochs, fit_stats['train_loss'], fit_stats['valid_loss'], model_name = model_name)
+plot_metric('accuracy', model_epochs, fit_stats['train_acc'], fit_stats['valid_acc'], model_name = model_name)
