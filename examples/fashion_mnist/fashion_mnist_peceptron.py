3403d0e85eddf640893177b49a24bad14547af17
Jefkine <jefkine@gmail.com>
2018-12-19 02:40:42 +0300

    tab edits

--- examples/fashion_mnist/fashion_mnist_peceptron.py
+++ examples/fashion_mnist/fashion_mnist_peceptron.py
@@ -19,8 +19,8 @@ plot_img_samples(train_data[:40], unhot(train_label[:40]), dataset = 'mnist')
 opt = register_opt(optimizer_name = 'adam', momentum = 0.01, learning_rate = 0.001)
 
 # model definition
-model       = Perceptron(epochs = 600, activation = 'relu', loss = 'cce', init_method = 'he_normal', optimizer =  opt)
-fit_stats   = model.fit(train_data, train_label)
+model     = Perceptron(epochs = 600, activation = 'relu', loss = 'cce', init_method = 'he_normal', optimizer = opt)
+fit_stats = model.fit(train_data, train_label)
 
 predictions = unhot(model.predict(test_data))
 print_results(predictions, unhot(test_label))
