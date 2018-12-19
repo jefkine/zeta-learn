6d61453e68ee366e5b25e7832ef29fc5f7a4fec9
Jefkine <jefkine@gmail.com>
2018-12-19 14:23:37 +0300

    typo edits

--- examples/cifar_100/cifar_100_pca.py
+++ examples/cifar_100/cifar_100_pca.py
@@ -11,4 +11,4 @@ transformed_data       = data.data.reshape(-1, transformed_image_dims).astype('f
 pca        = PCA(n_components = 2)
 components = pca.fit_transform(transformed_data)
 
-plot_pca(components, n_components = 2, colour_array = data.target, model_name = 'CIFAR-10 PCA')
+plot_pca(components, n_components = 2, colour_array = data.target, model_name = 'CIFAR-100 PCA')
