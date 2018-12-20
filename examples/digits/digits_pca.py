f27d44d35f2ea240491289a3d801b27539e2c881
Jefkine <jefkine@gmail.com>
2018-12-20 15:05:40 +0300

    edit variable - uppercase

--- examples/digits/digits_pca.py
+++ examples/digits/digits_pca.py
@@ -9,4 +9,4 @@ data = fetch_digits()
 pca        = PCA(n_components = 2)
 components = pca.fit_transform(data.data)
 
-plot_pca(components, n_components = 2, colour_array = data.target, model_name = 'Digits PCA')
+plot_pca(components, n_components = 2, colour_array = data.target, model_name = 'DIGITS PCA')
