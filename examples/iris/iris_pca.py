6acf15128282238a35a6feb85f445e96e4e279ea
Jefkine <jefkine@gmail.com>
2018-12-31 00:36:27 +0300

    iris pca model

--- examples/iris/iris_pca.py
+++ examples/iris/iris_pca.py
@@ -2,11 +2,12 @@
 
 from ztlearn.utils import plot_pca
 from ztlearn.ml.decomposition import PCA
-from ztlearn.datasets.mnist import fetch_mnist
+from ztlearn.datasets.iris import fetch_iris
 
-mnist = fetch_mnist()
+# fetch dataset
+data = fetch_iris()
 
 pca        = PCA(n_components = 2)
-components = pca.fit_transform(mnist.data.astype('float64'))
+components = pca.fit_transform(data.data.astype('float64'))
 
-plot_pca(components, n_components = 2, colour_array = mnist.target.astype('int'), model_name = 'MNIST PCA')
+plot_pca(components, n_components = 2, colour_array = data.target.astype('int'), model_name = 'IRIS PCA')
