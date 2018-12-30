ec8f6cfffd4f2e75c32648c79ce973d1a9c2a398
Jefkine <jefkine@gmail.com>
2018-12-31 00:55:54 +0300

    pima indians pca

---
+++ examples/pima_indians/pima_indians_pca.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from ztlearn.utils import plot_pca
+from ztlearn.ml.decomposition import PCA
+from ztlearn.datasets.pima import fetch_pima_indians
+
+# fetch dataset
+data = fetch_pima_indians()
+
+pca        = PCA(n_components = 2)
+components = pca.fit_transform(data.data[:,[2,3,4,5]].astype('float64'))
+
+plot_pca(components, n_components = 2, colour_array = data.target.astype('int'), model_name = 'PIMA INDIANS PCA')
