41a72f7bb9abba738ccb348f6fd8e4b0b720d0fe
Jefkine <jefkine@gmail.com>
2019-02-17 14:59:34 +0300

    added new text util modules

--- ztlearn/utils/text_utils.py
+++ ztlearn/utils/text_utils.py
@@ -58,3 +58,61 @@ def gen_char_sequence_xtyt(text, maxlen, step, tensor_dtype = np.int):
             y[i, t, char_to_indices[char]] = 1
 
     return x, y, len_chars
+
+
+def pad_sequence(sequence,
+                           maxlen     = None,
+                           dtype      = 'int32',
+                           padding    = 'pre',
+                           truncating = 'pre',
+                           value      = 0.0):
+    if (maxlen is None):  return sequence
+
+    np_sequence   = np.array(sequence)
+    sequence_size = np_sequence.size
+
+    if (maxlen > sequence_size):
+        pad_size = maxlen - sequence_size
+        if (padding == 'pre'):  front_pad, back_pad = pad_size, 0
+        if (padding == 'post'): front_pad, back_pad = 0, pad_size
+
+        paded = np.pad(sequence,
+                                 (front_pad, back_pad),
+                                 'constant',
+                                 constant_values = (value, value))
+        return paded
+
+    if (sequence_size > maxlen):
+        trunc_size = sequence_size - maxlen
+        if (truncating == 'pre'):  truncated = np_sequence[:-trunc_size]
+        if (truncating == 'post'): truncated = np_sequence[trunc_size:]
+
+        return truncated
+
+    if(sequence_size == maxlen): return np_sequence
+
+
+def get_sentence_tokens(text_list, maxlen = None, dtype = 'int32'):
+    unique_words = list(set(text_list.split())) # get the unique words
+
+    import re
+    sentences = re.split(r'(?<=[.?!])\s+', text_list)
+
+    if maxlen is None:
+        maxlen = max(longest_sentence(sentences))
+
+    sentence_index_list = []
+    for _, s in enumerate(sentences):
+        sentence_index = list(map(unique_words.index, s.split()))
+        padded_index   = pad_sequence(sentence_index, maxlen)
+        sentence_index_list.append(padded_index)
+
+    return np.array(sentence_index_list), len(unique_words), maxlen
+
+
+def longest_sentence(sentences):
+    ''' find longest sentence in a list of sentences '''
+    if isinstance(sentences, list):
+        yield len(sentences)
+        for sentence in sentences:
+            yield from longest_sentence(sentence)
