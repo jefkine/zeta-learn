dae51587b3d8a64960abd90e66f904f55aaa423a
Jefkine <jefkine@gmail.com>
2019-02-17 17:20:18 +0300

    add sentiment analysis rnn

---
+++ examples/text/word_embedding/sentiment_analysis_rnn.py
@@ -0,0 +1,62 @@
+from ztlearn.utils import *
+from ztlearn.dl.models import Sequential
+from ztlearn.optimizers import register_opt
+from ztlearn.utils import get_sentence_tokens
+from ztlearn.dl.layers import Embedding, Flatten, Dense, RNN
+
+text_list = [
+    'I like rally cars.',
+    'This is good.',
+    'This is bad.',
+    'Rainy days are the worst.',
+    'Mercedes is a good brand.',
+    'Nobody has the patience for bad food.',
+    'I believe Maasai Mara is the best.',
+    'This is definately a bad idea.',
+    'The trains in the city center suck!',
+    'I have a new shinny app love it!',
+    'Roads here are bad.',
+    'The hospital has good service.',
+]
+
+paragraph = ' '.join(text_list)
+sentences_tokens, vocab_size, longest_sentence = get_sentence_tokens(paragraph)
+sentence_targets = one_hot(np.array([1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1]))
+
+train_data, test_data, train_label, test_label = train_test_split(sentences_tokens,
+                                                                  sentence_targets,
+                                                                  test_size   = 0.2,
+                                                                  random_seed = 5)
+
+# optimizer definition
+opt = register_opt(optimizer_name = 'rmsprop', momentum = 0.1, learning_rate = 0.001)
+
+model = Sequential()
+model.add(Embedding(vocab_size, 4, input_length = longest_sentence))
+model.add(RNN(5, activation = 'tanh', bptt_truncate = 2, input_shape = (2, longest_sentence)))
+model.add(Flatten())
+model.add(Dense(2, activation = 'softmax'))
+model.compile(loss = 'mse', optimizer = opt)
+
+model.summary('embedded sentences rnn')
+
+'''
+NOTE:
+batch size should be equal the size of embedding
+vectors and divisible by the training set size
+'''
+
+model_epochs = 500
+fit_stats = model.fit(train_data,
+                      train_label,
+                      batch_size = 4,
+                      epochs     = model_epochs,
+                      validation_data = (test_data, test_label))
+
+model_name = model.model_name
+plot_metric('loss', model_epochs, fit_stats['train_loss'], fit_stats['valid_loss'], model_name = model_name)
+plot_metric('accuracy', model_epochs, fit_stats['train_acc'], fit_stats['valid_acc'], model_name = model_name)
+
+# test out with the first sentence - sentences_tokens[0]
+output_array = model.predict(np.expand_dims(sentences_tokens[0], axis=0))
+print(np.argmax(output_array))
