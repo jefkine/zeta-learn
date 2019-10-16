1ba4b084487861d065ad7027c838fa574196cd8c
Jefkine <jefkine@gmail.com>
2019-10-16 19:30:52 +0100

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import io
 import os
 import sys
-from setuptools import setup
+from setuptools import setup, Extension
 from setuptools import find_packages
 
 NAME            = 'ztlearn'
@@ -30,18 +30,19 @@ else:
     about['__version__'] = VERSION
 
 setup(
-    name                 = NAME,
-    version              = about['__version__'],
-    description          = DESCRIPTION,
-    long_description     = long_description,
-    author               = AUTHOR,
-    author_email         = EMAIL,
-    python_requires      = REQUIRES_PYTHON,
-    url                  = URL,
-    install_requires     = REQUIRED,
-    include_package_data = True,
-    license              = 'MIT',
-    classifiers          = [
+    name                          = NAME,
+    version                       = about['__version__'],
+    description                   = DESCRIPTION,
+    long_description              = long_description,
+    long_description_content_type = 'text/markdown',
+    author                        = AUTHOR,
+    author_email                  = EMAIL,
+    python_requires               = REQUIRES_PYTHON,
+    url                           = URL,
+    install_requires              = REQUIRED,
+    include_package_data          = True,
+    license                       = 'MIT',
+    classifiers                   = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
